5e431db9cf69558a4e9f11424e44fbbfd919ed4f
Yizack Rangel <yizackr@gmail.com>
2022-12-04 22:10:46 -0500

    Cloudflare Cache API

--- src/index.js
+++ src/index.js
@@ -1,36 +1,41 @@
 import InstagramFeed from "./InstagramFeed.js";
 
 class JsonResponse extends Response {
-  constructor(body, domain) {
+  constructor(body) {
     const jsonBody = JSON.stringify(body);
     const options = {
       headers: {
         "Content-Type": "application/json;charset=UTF-8",
-        "Access-Control-Allow-Credentials": true,
-        "Access-Control-Allow-Origin": domain,
-        "Set-Cookie": "refreshed=true; Max-Age=86400; SameSite=None; Secure; HttpOnly"
+        "Access-Control-Allow-Origin": "*",
+        "Cache-Control": "s-maxage=86400"
       }
     };
     super(jsonBody, options);
   }
 }
 
-async function handleRequest(API, env, refreshed) {
+async function handleRequest(req, ctx, API) {
+  const cacheUrl = new URL(req.url);
+  const cacheKey = new Request(cacheUrl.toString(), req);
+  const cache = caches.default;
+  let response = await cache.match(cacheKey);
+
+  if (response) {
+    console.log("Found in cache!");
+    return response;
+  }
+
+  API.refreshAccessToken();
   const { data } = await API.getFeed();
-  return new JsonResponse({refreshed, data}, env.DOMAIN);
+  response = new JsonResponse({data});
+  console.log("Stored in cache!");
+  ctx.waitUntil(cache.put(cacheKey, response.clone()));
+  return response;
 }
 
 export default {
-  async fetch(req, env) {
-    const API = new InstagramFeed(env.ACCESS_TOKEN);
-    const { headers } = req;
-    const cookies = headers.get("Cookie") || "";
-    const refreshed = cookies.includes("refreshed=true");
-
-    if (!refreshed) {
-      API.refreshAccessToken();
-    }
-    
-    return await handleRequest(API, env, refreshed);
+  async fetch(req, env, ctx) {
+    const API = new InstagramFeed(env.ACCESS_TOKEN);    
+    return await handleRequest(req, ctx, API);
   }
 };
