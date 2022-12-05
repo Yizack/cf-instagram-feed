9e2b351336c48e7b3ec673b71213422cf20f5d33
Yizack Rangel <yizackr@gmail.com>
2022-12-05 01:07:50 -0500

    Router handle, cache global, fetch fix

--- src/InstagramFeed.js
+++ src/InstagramFeed.js
@@ -2,18 +2,24 @@ class InstagramFeed {
 
   constructor(access_token) {
     this.api = "https://graph.instagram.com";
-    this.access_token = access_token;
+    this.access_token = access_token || null;
   }
 
   async refreshAccessToken() {
     const response = await fetch(`${this.api}/refresh_access_token?grant_type=ig_refresh_token&access_token=${this.access_token}`);
-    const { access_token } = await response.json() || this.access_token;
-    this.access_token = access_token;
+    if (response.ok) {
+      const { access_token } = await response.json();
+      this.access_token = access_token;
+    }
   }
 
   async getFeed() {
     const response = await fetch(`${this.api}/me/media?fields=username,permalink,timestamp,caption&access_token=${this.access_token}`);
-    return response.json() || null;
+    let res = {};
+    if (response.ok) {
+      res = response.json();
+    }
+    return res;
   }
 
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
+import { Router } from "itty-router";
 import InstagramFeed from "./InstagramFeed.js";
+
+const router = Router();
 
 class JsonResponse extends Response {
   constructor(body) {
@@ -14,30 +17,34 @@ class JsonResponse extends Response {
   }
 }
 
-async function handleRequest(req, ctx, API) {
-  const cacheUrl = new URL(req.url);
-  const cacheKey = new Request(cacheUrl.toString(), req);
-  const cache = caches.default;
-  let response = await cache.match(cacheKey);
-
-  if (response) {
-    console.log("Found in cache!");
-    return response;
-  }
-
-  API.refreshAccessToken();
+router.get("/", async (req, ctx, API, cacheManager) => {
+  const { cache, cacheKey } = cacheManager;
   const { data } = await API.getFeed();
-  response = new JsonResponse({data});
+  let response = new JsonResponse({data});
   if (data) {
     console.log("Stored in cache!");
     ctx.waitUntil(cache.put(cacheKey, response.clone()));
   }
   return response;
-}
+});
+
+router.all("*", () => new Response("Not Found.", { status: 404 }));
 
 export default {
   async fetch(req, env, ctx) {
-    const API = new InstagramFeed(env.ACCESS_TOKEN);    
-    return await handleRequest(req, ctx, API);
+    const cacheUrl = new URL(req.url);
+    const cacheKey = new Request(cacheUrl.toString(), req);
+    const cache = caches.default;
+    const cacheManager = { cache, cacheKey };
+    const response = await cache.match(cacheKey);
+
+    if (response) {
+      console.log("Found in cache!");
+      return response;
+    }
+
+    const API = new InstagramFeed(env.ACCESS_TOKEN); 
+    await API.refreshAccessToken();   
+    return router.handle(req, ctx, API, cacheManager);
   }
 };
