73918a9f0a36be12f3e5f1aad1f1368c5572741a
Yizack Rangel <yizackr@gmail.com>
2024-07-12 23:49:55 -0500

    chore: cached handler

--- src/routes/index.get.ts
+++ src/routes/index.get.ts
@@ -1,9 +1,10 @@
-import { defineEventHandler, type H3Event } from "h3";
+import type { H3Event } from "h3";
+import { defineCachedEventHandler } from "../imports";
 import InstagramFeed from "../InstagramFeed";
 
-export default defineEventHandler(async (event: H3Event) => {
+export default defineCachedEventHandler(async (event: H3Event) => {
   const API = new InstagramFeed(event.context.cloudflare.env.ACCESS_TOKEN);
   await API.refreshAccessToken();
   const data = await API.getFeed();
   return { data };
-});
+}, { maxAge: 86400 });
